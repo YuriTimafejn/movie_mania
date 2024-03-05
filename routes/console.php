06b424942d4567fdee22f955afb802d5fe4fbfb8
Yuri Timafejn <timafejn@gmail.com>
2024-03-05 04:14:39 -0300

    chore: included artisan command to clear log files

--- routes/console.php
+++ routes/console.php
@@ -3,17 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+Artisan::command('log:clear', function () {
+    exec('rm -f ' . storage_path('logs/*.log'));
+    exec('rm -f ' . base_path('*.log'));
+    $this->comment('Log files have been cleared');
+})->describe('Clear log files');
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
